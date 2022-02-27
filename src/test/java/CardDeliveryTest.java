6100adaa806df05d56aa002b92a444ec3183c4ae
taniakku <tatiennekzm@gmail.com>
2022-02-27 21:16:37 +0300

    changes for date

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -3,22 +3,26 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
+import java.time.LocalDate;
 
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static java.time.LocalDate.now;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CardDeliveryTest {
+   LocalDate date = now();
+   LocalDate order = date.plusDays(3);
 
     @Test
     void ShouldSendForm() {
         Configuration.holdBrowserOpen = true;
 
         open("http://0.0.0.0:9999");
-        $("[placeholder='Город']").val("Санкт-Петербург");
-        $("[placeholder='Дата встречи']").val("02.03.2022");
+        $("[data-test-id='city'] input").val("Санкт-Петербург");
+        $("[data-test-id='date'] input").setValue(String.valueOf(order));
         $(byName("name")).val("Пушкина Анна-Мария");
         $("[name='phone']").val("+79119786655");
         $(byClassName("checkbox__box")).click();
