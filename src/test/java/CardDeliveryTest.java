22370412e059d838b7513a9ba8b06af0ed0d7092
taniakku <tatiennekzm@gmail.com>
2022-03-02 20:16:04 +0300

    rework

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -4,31 +4,36 @@ import org.junit.jupiter.api.Test;
 
 import java.time.Duration;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static java.time.LocalDate.now;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CardDeliveryTest {
-   LocalDate date = now();
-   LocalDate order = date.plusDays(3);
+
+    String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
     @Test
     void ShouldSendForm() {
         Configuration.holdBrowserOpen = true;
 
-        open("http://0.0.0.0:9999");
+        String orderDate = generateDate(3);
+
+        open("http://localhost:9999");
         $("[data-test-id='city'] input").val("Санкт-Петербург");
-        $("[data-test-id='date'] input").setValue(String.valueOf(order));
+        $("[data-test-id='date'] input").setValue(orderDate);
         $(byName("name")).val("Пушкина Анна-Мария");
         $("[name='phone']").val("+79119786655");
         $(byClassName("checkbox__box")).click();
         $(byText("Забронировать")).click();
 
         $(withText("Успешно!")).should(appear, Duration.ofSeconds(15));
+        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + orderDate), Duration.ofSeconds(15));
+
 
     }
 
